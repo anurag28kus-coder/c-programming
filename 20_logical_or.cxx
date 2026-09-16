0672389ca100971088fbca22214c01513d53bcc6
Anurag Kushwaha <anurag28kus@gmail.com>
2026-09-16 12:43:30 +0530

    Add logical OR program

---
+++ 20_logical_or.cxx
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int marks = 35 ;
+    int specialPermission = 1 ;
+    
+    if (marks >= 40 || specialPermission==1)
+{
+      printf ("Allowed");
+}   else { 
+      printf (" Not Allowed");
+}
+
+return 0;
+}
+    
