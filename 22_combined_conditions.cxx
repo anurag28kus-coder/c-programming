1a427cecd6806d9a6d29477a1849d5dec15308f3
Anurag Kushwaha <anurag28kus@gmail.com>
2026-09-16 16:29:51 +0530

    Add combined conditions program

---
+++ 22_combined_conditions.cxx
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+    int marks = 85 ; 
+    int attendence = 80 ;
+    
+    if ( marks >= 40 && attendence >= 75){
+        printf("Pass and Good Attendence ");
+ 
+       }else {
+           printf("Condition Not Met ");
+       }
+       return 0;
+}
