924c50e9bcdb8c6e306d9c28fef37c15e90f544d
Anurag Kushwaha <anurag28kus@gmail.com>
2026-09-16 12:49:45 +0530

    Add logical NOT program

---
+++ 21_logical_not.cxx
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int value = 0;
+    
+    if (!value){
+        printf("Value is false ");
+    } else { 
+       printf("Value is true ");
+    }
+    
+    return 0;
+}
